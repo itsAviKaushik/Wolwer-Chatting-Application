355b5fe8bff417416e379fd8fb97337df0d13abe
Avichal Kaushik <90247880+itsAviKaushik@users.noreply.github.com>
2022-12-23 21:53:52 +0530

    Configured Environment Variable and added Author Snippets!

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+// | ----------------------------------------------------------- |
+// | ----------------------------------------------------------- |
+// | --- Project Name :- Unicorn   ----------------------------- |
+// | --- Author :- Avichal Kaushik ----------------------------- |
+// | --- Author Email :- avichalkaushik0007@gmail.com ---------- |
+// | ----------------------------------------------------------- |
+// | ----------------------------------------------------------- |
+
+// Configuring the environmental variables.
+require("dotenv").config();
+
 const app = require("./app");
 
 const server = app.listen(process.env.PORT, () => {
-    console.log("Server Listening!");
+    console.log(`Server Listening on ${process.env.APP_HOME_URL} !`);
 });
