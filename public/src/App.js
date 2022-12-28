f67fa3c59dd9d0570b2100ca5fe411e095593ddf
Avichal Kaushik <90247880+itsAviKaushik@users.noreply.github.com>
2022-12-28 22:20:07 +0530

    Added React Router and Created Login, Signup, Chatroom UI!

--- public/src/App.js
+++ public/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Signup from './components/user/Signup';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/user/Login';
+import Chatroom from './components/chat/Chatroom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route exact path='/' element={<Chatroom />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
